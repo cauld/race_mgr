e7f30a7ede43dd6199f3a6928270876446e6cabc
Chad Auld <chadauld@gmail.com>
2022-04-16 11:34:05 -0600

    Initial CORS config

--- server/src/main/java/com/coop/racemgr/WebConfig.java
+++ server/src/main/java/com/coop/racemgr/WebConfig.java
@@ -1,2 +1,16 @@
-package com.coop.racemgr;public class WebConfig {
+package com.coop.racemgr;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 }
