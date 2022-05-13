c03e69a2df0fc106b2b9e25d3476cf4a7af3af1c
Chad Auld <chadauld@gmail.com>
2022-05-12 19:46:45 -0600

    Fix for Rotation query object reactivation from db. Revisit later.

---
+++ server/src/main/java/com/coop/racemgr/MongoDBConfig.java
@@ -0,0 +1,23 @@
+package com.coop.racemgr;
+
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+
+// @TODO: Revisit this. It disables JPA _class assignment on serialization for save.
+// We'd have to rework RaceRotationConfig to sep the model and the rotation maker
+@Configuration
+public class MongoDBConfig implements InitializingBean {
+    @Autowired
+    @Lazy
+    private MappingMongoConverter mappingMongoConverter;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        mappingMongoConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
+    }
+}
