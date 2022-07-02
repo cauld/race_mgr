7fa83a5b983a3fca41b49535d455d0c443afc12d
Chad Auld <chadauld@gmail.com>
2022-07-02 15:33:57 -0600

    CORS/Client proxy updates. Spring boot update as well.

--- src/main/java/com/coop/racemgr/WebConfig.java
+++ src/main/java/com/coop/racemgr/WebConfig.java
@@ -11,7 +11,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000");
     }
 
     @Override
